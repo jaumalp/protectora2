a45df9d46021dcd090e0c52ba99d006b3f2bdab5
Jaime <Jaumalp@gmail.com>
2019-01-02 14:46:14 +0100

    '0.3'

---
+++ database/migrations/2019_01_02_143520_create_noticias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('texto');
+            $table->integer('importancia');
+            $table->unsignedInteger('animal_id');
+            $table->foreign('animal_id')->references('id')->on('animals');
+            $table->integer('fotoAdjunta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticias');
+    }
+}
